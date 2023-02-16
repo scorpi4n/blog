504b3a21bc393d66945473394a0ad1e007c5d4fe
James Phillips <scorpi4n@scorpi4n.com>
2023-02-16 11:30:11 -0600

    Create create base blog

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,22 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
-	schema: z.object({
+	schema: z.strictObject({
 		title: z.string(),
 		description: z.string(),
+		tags: z.array(z.enum(["web dev", "IT"])),
+		draft: z.boolean(),
 		// Transform string to Date object
 		pubDate: z
 			.string()
 			.or(z.date())
-			.transform((val) => new Date(val)),
+			.transform(val => new Date(val)),
 		updatedDate: z
 			.string()
+			.or(z.date())
 			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
+			.transform(str => (str ? new Date(str) : undefined)),
 	}),
 });
 
